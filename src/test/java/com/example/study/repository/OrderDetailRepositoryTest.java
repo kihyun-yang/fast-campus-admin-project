e7085835b7cf5fa05c491c776c3d0f75cf18a9ed
양기현 <rlgusdid@naver.com>
2021-05-15 11:54:36 +0900

    13.JPA 연관관계 설정 배우기 - 코딩하기 - 1

---
+++ src/test/java/com/example/study/repository/OrderDetailRepositoryTest.java
@@ -0,0 +1,31 @@
+package com.example.study.repository;
+
+import com.example.study.StudyApplicationTests;
+import com.example.study.model.entity.OrderDetail;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import java.time.LocalDateTime;
+
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
+public class OrderDetailRepositoryTest extends StudyApplicationTests {
+
+    @Autowired
+    private OrderDetailRepository orderDetailRepository;
+
+    @Test
+    public void create() {
+        OrderDetail orderDetail = new OrderDetail();
+
+        orderDetail.setOrderAt(LocalDateTime.now());
+
+        orderDetail.setUserId(1L);
+
+        orderDetail.setItemId(1L);
+
+        OrderDetail newOrderDetail = orderDetailRepository.save(orderDetail);
+        assertNotNull(newOrderDetail);
+    }
+
+}
