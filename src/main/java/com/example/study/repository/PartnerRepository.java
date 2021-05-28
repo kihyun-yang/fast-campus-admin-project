644ae0293e316c08a3e8244dea709906f2be35d1
양기현 <rlgusdid@naver.com>
2021-05-28 13:44:45 +0900

    10-01. 샘플 데이터 생성하기

--- src/main/java/com/example/study/repository/PartnerRepository.java
+++ src/main/java/com/example/study/repository/PartnerRepository.java
@@ -1,7 +1,11 @@
 package com.example.study.repository;
 
+import com.example.study.model.entity.Category;
 import com.example.study.model.entity.Partner;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface PartnerRepository extends JpaRepository<Partner, Long> {
+    List<Partner> findByCategory(Category category);
 }
